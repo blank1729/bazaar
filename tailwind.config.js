d75165f03ff9ba36a5d077e35afd13ee5061894e
blank <85779539+blank1729@users.noreply.github.com>
2023-05-24 02:24:05 +0530

    first commit, not sure if it's working since it's too damn slow

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF0000', // Replace with your primary color
+        secondary: '#00FF00', // Replace with your secondary color
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      darkMode: 'class',
+    },
+  },
+  plugins: [],
+}
