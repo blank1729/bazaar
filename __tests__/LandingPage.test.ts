592cde1a24e22e9eccada53d59b889a8434645d8
blank <85779539+blank1729@users.noreply.github.com>
2023-05-24 18:43:21 +0530

    added github ci

---
+++ __tests__/LandingPage.test.ts
@@ -0,0 +1,6 @@
+import React from "react";
+import { expect, test } from "vitest";
+import { render, screen, within } from "@testing-library/react";
+import Header from "../components/common/Header";
+
+test("testing header", () => {});
